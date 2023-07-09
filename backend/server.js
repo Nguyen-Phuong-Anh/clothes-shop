95438abd0018daebe06b7de2ee73fb52aead7ff7
Phuong-Anh <nguyenphuonganh130e0@gmail.com>
2023-07-09 15:20:03 +0700

    using normal react component instead of handlebars

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const connectDB = require('./connectDB')
 const route = require('./routes/route')
-const handlebars = require('handlebars')
-const { engine } = require('express-handlebars')
-const path = require('path')
 
 const app = express()
 
@@ -13,9 +9,6 @@ connectDB()
 
 app.use(express.json())
 app.use(cors())
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, '/views'));
 
 route(app)
 
