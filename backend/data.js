983bc211199c31ed71920e4fe5d17d7e549f5294
Phuong-Anh <nguyenphuonganh130e0@gmail.com>
2023-07-14 15:57:57 +0700

    fsdf

--- backend/data.js
+++ backend/data.js
@@ -24,6 +24,17 @@ const data = {
         },
         {
             category: 'clothes', //shoes or clothes
+            name: 't-collar shirt', 
+            type: 'shirt', // shirts, sneakers...
+            sizes: ['xl', 'm', 'l'],
+            colors: ['red', 'blue'],
+            material: 'cotton',
+            description: 'very good',
+            number: 4239, //number of remain products
+            price: 1290
+        },
+        {
+            category: 'clothes', //shoes or clothes
             name: 't shirt', 
             type: 'shirt', // shirts, sneakers...
             sizes: ['xl', 'm', 'l'],
@@ -32,7 +43,29 @@ const data = {
             description: 'very good',
             number: 4290, //number of remain products
             price: 3288
-        }
+        },
+        {
+            category: 'clothes', //shoes or clothes
+            name: 't shirt', 
+            type: 'shirt', // shirts, sneakers...
+            sizes: ['xl', 'm', 'l'],
+            colors: ['red', 'orange'],
+            material: 'polystyrene',
+            description: 'very good',
+            number: 4290, //number of remain products
+            price: 3288
+        },
+        {
+            category: 'clothes', //shoes or clothes
+            name: 't shirt', 
+            type: 'shirt', // shirts, sneakers...
+            sizes: ['xl', 'm', 'l'],
+            colors: ['red', 'orange'],
+            material: 'polystyrene',
+            description: 'very good',
+            number: 4290, //number of remain products
+            price: 3288
+        },
     ]
 }
 
