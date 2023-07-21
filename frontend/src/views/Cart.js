7666e55819220bb42eab6641b2908980db2cdc7f
Phuong-Anh <nguyenphuonganh130e0@gmail.com>
2023-07-21 10:39:21 +0700

    select all

--- frontend/src/views/Cart.js
+++ frontend/src/views/Cart.js
@@ -13,51 +13,69 @@ function Cart() {
     const handleRotate = () => {
         setRotate(!rotate)
     }
+
+    function handleCheck() {
+        const elems = document.getElementsByClassName('checkbox')
+        for(let elem of elems) {
+            elem.checked = !(elem.checked)
+        }
+    }
+
     return (
         <div>
             <table className={styles.table}>
-                <tr className={styles.header}>
-                    <th>
-                    <div className={styles.checkAll}>
-                        <input id="checkAll" type="checkbox"/>
-                        <label for="checkAll"></label>
-                        <p>Product</p>
-                    </div>
-                    </th>
-                    <th>Price</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                    <th>Option</th>
-                </tr>
+                <thead>
+                    <tr className={styles.header}>
+                        <th>
+                        <div className={styles.checkAll}>
+                            <input
+                            id="checkAll"
+                            type="checkbox"
+                            onClick={handleCheck}
+                            />
+                            <label htmlFor="checkAll"></label>
+                            <p>Product</p>
+                        </div>
+                        </th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Total</th>
+                        <th>Option</th>
+                    </tr>
+                </thead>
 
-                <tr className={styles.wrapper}>
-                    <td>
-                        <div className={styles.item}>
+                <tbody>
+                    <tr className={styles.wrapper}>
+                        <td>
+                            <div className={styles.item}>
                             <div className={styles.checkBox_wrapper}>
-                                <input type="checkbox" id='checkItem' />
-                                <label for="checkItem"></label>
-                            </div>
-                            <div className={styles.itemBody}>
-                                <img src={image3}/>
-                                <p>fsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfj</p>
-                            </div>
-                            <div className={styles.itemInfo}>
-                                <p id='type' className={`${styles.type} ${rotate ? styles.rotate : ''}`} onClick={handleRotate}>Item's type</p>
-                                <p>collar shirt - m - red</p>
-                                <div className={`${styles.popUp} ${rotate ? styles.show : ''}`}>
-                                    <p>Color</p>
-                                    <p>Size</p>
+                                <input className='checkbox' type="checkbox" id='checkItem' />
+                                    <label htmlFor="checkItem"></label>
+                                </div>
+                                <div className={styles.itemBody}>
+                                    <img src={image3}/>
+                                    <p>fsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfjfsfj</p>
+                                </div>
+                                <div className={styles.itemInfo}>
+                                    <p id='type' className={`${styles.type} ${rotate ? styles.rotate : ''}`} onClick={handleRotate}>Item's type</p>
+                                    <p>collar shirt - m - red</p>
+                                    <div className={`${styles.popUp} ${rotate ? styles.show : ''}`}>
+                                        <p>Color</p>
+                                        <p>Size</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </td>
-                    <td>price</td>
-                    <td className={styles.center}><SnippetQuantity number={number} setNumber={setNumber}/></td>
-                    <td>total</td>
-                    <td className={styles.delete}>
-                        <FontAwesomeIcon icon={faTrashCan} /> Delete
-                    </td>
-                </tr>
+                        </td>
+                        <td>price</td>
+                        <td className={styles.center}><SnippetQuantity number={number} setNumber={setNumber}/></td>
+                        <td>total</td>
+                        <td>
+                            <div className={styles.delete}>
+                                <FontAwesomeIcon icon={faTrashCan} /> Delete
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
