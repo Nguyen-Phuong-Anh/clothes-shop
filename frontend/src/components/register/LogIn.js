7e5d530d2467e4dcd25cd3b4659d7b609e6ede62
Phuong-Anh <nguyenphuonganh130e0@gmail.com>
2023-07-22 10:07:30 +0700

    align display way 1

--- frontend/src/components/register/LogIn.js
+++ frontend/src/components/register/LogIn.js
@@ -1,25 +1,44 @@
 import styles from './LogIn.module.css'
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function LogIn() {
+    const [hideLogIn, setHideLogIn] = useState(false)
+    const [hideRegister, setHideRegister] = useState(true)
+
+    const handleLogIn = () => {
+        if(hideLogIn == true) {
+            setHideLogIn(false)
+            setHideRegister(true)
+        }
+    }
+
+    const handleRegister = () => {
+        if(hideRegister == true) {
+            setHideLogIn(true)
+            setHideRegister(false)
+        }
+    }
+
     return (
         <div className={styles.wrapper}>
             <h1>RAYNE</h1>
             <div className={styles.choice}>
-                <button>LOGIN</button>
-                <button>REGISTER</button>
+                <button onClick={handleLogIn}>LOGIN</button>
+                <button onClick={handleRegister}>REGISTER</button>
             </div>
 
             <div className={styles.body}>
-                <div className={styles.login}>
+                <div className={`${styles.login} ${hideLogIn ? styles.show : ''}`}>
                     <div>
                         <div>
                             <Form.Label htmlFor='inputEmail'>Email address</Form.Label>
-                            <Form.Control id='inputEmail' type="email" />
+                            <Form.Control className={`${styles.input} p-2`} id='inputEmail' type="email" />
                         </div>
                         <div>
                             <Form.Label className='mt-3' htmlFor="inputPassword5">Password</Form.Label>
                             <Form.Control
+                                className={`${styles.input} p-2`}
                                 type="password"
                                 id="inputPassword5"
                                 aria-describedby="passwordHelpBlock"
@@ -30,15 +49,51 @@ function LogIn() {
                         <a href='/'>Forgot password?</a>
                     </div>
                     <div>
-                        <button className={styles.sigIn_btn}>SIGN IN</button>
+                        <button className={styles.btn}>SIGN IN</button>
                     </div>
                     <div>
                         <p>Do not have account? <span><a href='/'>Register</a></span></p>
                     </div>
                 </div>
 
-                <div className={styles.register}>
+                <div className={`${styles.register} ${hideRegister ? styles.show : ''}`}>
+                    <div>
+                        <Form.Control className={`${styles.input} p-2`} placeholder='Name' />
+                    </div>
 
+                    <div>
+                        <Form.Control className={`${styles.input} p-2`} placeholder='Username' />
+                    </div>
+
+                    <div>
+                        <Form.Control className={`${styles.input} p-2`} type="email" placeholder='Email' />
+                    </div>
+                    
+                    <div>
+                        <Form.Control
+                            className={`${styles.input} p-2`}
+                            type="password"
+                            aria-describedby="passwordHelpBlock"
+                            placeholder='Password'
+                        />
+                    </div>
+                    <div>
+                        <Form.Control
+                            className={`${styles.input} p-2`}
+                            type="password"
+                            aria-describedby="passwordHelpBlock"
+                            placeholder='Repeat password'
+                        />
+                    </div>
+                    <div className={styles.term}>
+                        <input type="checkbox" id='checkTerm'
+                        />
+                        <label htmlFor='checkTerm'></label>
+                        <p>I have read and agree to the terms</p>
+                    </div>
+                    <div>
+                        <button className={styles.btn}>SIGN UP</button>
+                    </div>
                 </div>
             </div>
         </div>
