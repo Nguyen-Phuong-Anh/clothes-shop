c6820378c8a3e65f515058b1739c5e6a3f1c7c01
Phuong-Anh <nguyenphuonganh130e0@gmail.com>
2023-11-22 10:09:37 +0700

    fix 8

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ const axiosPrivate = axios.create({
     baseURL: 'https://clothes-shop-api.onrender.com',
     headers: { 'Content-Type': 'application/json'},
     withCredentials: true,
-
+    
 })
 
 export default axiosPrivate
